eaa227c8bf70b6c63e690cad51206e542db394d1
saunderl6368 <31601174+saunderl6368@users.noreply.github.com>
2017-09-03 20:02:04 -0400

    Add files via upload
    First submission- Module 2, Tutorial 1: Sales Prediction

---
+++ M2T1_SalesPrediction_Saunders.py
@@ -0,0 +1,11 @@
+# CTI-110
+# M2T1- Sales Prediction
+# Laura Saunders
+# 9/3/2017
+# Get the projected total sales.
+total_sales = float(input ("Enter the projected sales: "))
+# Calculate the annual profit as 23 percent of total sales.
+annual_profit = total_sales * 0.23
+# Display the annual profit.
+print ("The annual profit is $", format (annual_profit, ",.2f"))
+
