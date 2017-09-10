5fcbfd19af31acd687aa6c827b4618b74c83461d
saunderl6368 <31601174+saunderl6368@users.noreply.github.com>
2017-09-10 18:51:52 -0400

    Add files via upload
    Module 2 Homework 1 Distance Traveled

---
+++ M2HW1_DistanceTraveled_Saunders.py
@@ -0,0 +1,11 @@
+# CTI- 110
+# M2HW1- Distance Traveled
+# Laura Saunders
+# 9/3/2017
+#
+distanceAfter6= 70 * 6
+print ("The distance in miles the car will travel in 6 hours is", distanceAfter6)
+distanceAfter10= 70 * 10
+print ("The distance in miles the car will travel in 10 hours is", distanceAfter10)
+distanceAfter15= 70 * 15
+print ("The distance in miles the car will travel in 15 hours is", distanceAfter15)
