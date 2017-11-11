b053826406ba33db9d4a95a0648c929bda5fbb2f
saunderl6368 <31601174+saunderl6368@users.noreply.github.com>
2017-11-10 19:11:47 -0500

    Add files via upload
    M5LAB3_Turtle Graphics: Snowflake

---
+++ M5LAB3_Saunders.py
@@ -0,0 +1,16 @@
+# CTI- 110
+# M5LAB3- Snowflake
+# Laura Saunders
+# 10/21/2017
+
+import turtle
+
+def main():
+    for i in range(10):
+        for i in range(2):
+            turtle.forward(100)
+            turtle.right(60)
+            turtle.forward(100)
+            turtle.right(120)
+        turtle.right(36)
+main()
