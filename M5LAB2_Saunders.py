4e7650df5ff59973303b064796d2c77b702cc392
saunderl6368 <31601174+saunderl6368@users.noreply.github.com>
2017-11-10 19:11:09 -0500

    Add files via upload
    M5LAB2_Turtle Graphics: Initials

---
+++ M5LAB2_Saunders.py
@@ -0,0 +1,38 @@
+# CTI- 110
+# M5LAB2- Initials
+# Laura Saunders
+# 10/21/2017
+
+import turtle
+
+def main():
+    # Write a program using turtle graphics that displays first and last initials.
+    
+    turtle.pencolor("red")
+    turtle.pensize(10)
+    turtle.penup()
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.pendown()
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.penup()
+    turtle.forward(50)
+    turtle.pendown()
+    turtle.forward(50)
+    turtle.left(90)
+    turtle.forward(25)
+    turtle.left(90)
+    turtle.forward(50)
+    turtle.right(90)
+    turtle.forward(25)
+    turtle.right(90)
+    turtle.forward(50)
+    
+      
+main()
