baf0595a09aef2db35cd085a07d61735dabdc370
saunderl6368 <31601174+saunderl6368@users.noreply.github.com>
2017-11-10 19:15:39 -0500

    Add files via upload
    M6T1_Kilometer Converter

---
+++ M6T1_Saunders.py
@@ -0,0 +1,31 @@
+# CTI 110
+# M6T1- Kilometer Converter
+# Laura Saunders
+# November 10, 2017
+
+
+# This program converts kilometers to miles.
+CONVERSION_FACTOR = 0.6214
+
+# The main function gets a distance in kilometers and calls
+# the show_miles function to convert it.
+
+def main():
+    # Get the distance in kilometers.
+    kilometers = float(input("Enter a distance in kilometers: "))
+
+    #Display the distance converted to miles.
+    show_miles (kilometers)
+
+# The show_miles function converts the parameter km from
+# kilometers to miles and displays the result.
+
+def show_miles (km):
+    # Calculate miles.
+    miles = km * CONVERSION_FACTOR
+
+    # Display the miles.
+    print (km, "kilometers equals", miles, "miles.")
+
+# Call the main function.
+main()
