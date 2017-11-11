28784b3cbf04a2a26e4b64f39f636256247af477
saunderl6368 <31601174+saunderl6368@users.noreply.github.com>
2017-11-10 19:10:30 -0500

    Add files via upload
    M5LAB1_Turtle Graphics: Shapes

---
+++ M5LAB1_Saunders.py
@@ -0,0 +1,16 @@
+# CTI- 110
+# M5LAB1- Shapes
+# Laura Saunders
+# 10/21/2017
+
+import turtle
+
+def main():
+    for i in range(0,4):
+        turtle.forward(50)
+        turtle.left(90)
+    for i in range(0,3):
+        turtle.forward(50)
+        turtle.left(120)
+main()
+        
